4fcd928fdda7280d99bfc8c30fc444878b4a7f98
Noureddine Belguinan <nor15dine@gmail.com>
2025-01-24 16:20:49 +0100

    wip

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,62 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { fileURLToPath, URL } from 'node:url'
+import purgecss from 'vite-plugin-purgecss'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
+    plugins: [
+        react(),
+        purgecss({
+            content: ['./index.html', './src/**/*.{js,jsx}'],
+            safelist: {
+                standard: [
+                    'html',
+                    'body',
+                    /^fade/,
+                    /^show$/,
+                    /^modal/,
+                    /^btn/,
+                    /^dropdown/,
+                    /^nav/,
+                    /^card/,
+                    /^table/,
+                    'active',
+                    'collapsed',
+                    'collapse',
+                    'show',
+                ],
+                deep: [
+                    /dropdown-menu$/,
+                    /modal$/,
+                    /fade$/,
+                    /show$/,
+                    /^nav-/,
+                    /^navbar-/,
+                    /^card-/,
+                    /^btn-/,
+                    /^table-/
+                ],
+                greedy: [
+                    /^modal-/,
+                    /^btn-/,
+                    /^nav-/,
+                    /^navbar-/,
+                    /^card-/,
+                    /^table-/,
+                    /^show$/,
+                    /^active$/,
+                    /^fade$/,
+                    /^collapse$/
+                ]
+            }
+        })
+    ],
+    build: {
+        assetsInlineLimit: 0,
+        cssMinify: true,
+        cssCodeSplit: true
+    },
+    resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
         }
